7e9f8c75aff6081aca0c86d7948db2434d38cae9
AhmedOg <ahmedogbinto@gmail.com>
2022-04-18 13:14:03 -0400

    nuLL propriety and methode

---
+++ Planete.java
@@ -0,0 +1,18 @@
+public class Planete
+{
+
+    String nom;
+    String matiere;
+    int diametre;
+
+    void revolution()
+    {
+        System.out.println("Je suis la planete " + this.nom + " et je tourne autour de mon etoile");
+    }
+
+    void rotaion()
+    {
+        System.out.println("Je suis la planete " + this.nom + " et je tourne sur moi-meme");
+    }
+
+}
